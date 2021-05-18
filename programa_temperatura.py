00c891ba895addc2bbc66f34d988a2a7ba048a08
Daniel Farias <dan.tfarias@gmail.com>
2021-05-17 22:37:16 -0300

    Add files via upload

---
+++ programa_temperatura.py
@@ -0,0 +1,25 @@
+#v1
+'''
+userInput=input("Digite a temperatura atual: ")
+
+if int(userInput) < 7:
+    print("Congelando! " + userInput + "Cº")
+elif int(userInput) < 10:
+    print("Frio! " + userInput + "Cº")
+elif int(userInput) > 26:
+    print("Ótimo! " + userInput + "Cº")
+else:
+    print("Agradável " + userInput + "Cº")
+'''
+#v2
+userInput=input("Digite a temperatura atual: ")
+temp = int (userInput)
+
+if temp <7:
+    print("Congelando! " + userInput + "Cº")
+elif temp <10:
+    print("Frio! " + userInput + "Cº")
+elif temp >26:
+    print("Ótimo! " + userInput + "Cº")
+else:
+    print("Agradável " + userInput + "Cº")
