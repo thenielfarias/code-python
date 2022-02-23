877108687e41149aa42c5328c3c3032fd6a66319
Daniel Farias <dan.tfarias@gmail.com>
2022-02-22 23:37:37 -0300

    Delete webscraper_ex.py

--- webscraper_ex.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-navegador = webdriver.Chrome()
-
-navegador.get("https://ri.magazineluiza.com.br/")
-navegador.find_element_by_xpath('//*[@id="owl-destaques"]/div[1]/div/div[4]/div/a/p').click()
-navegador.find_element_by_xpath('//*[@id="qh+HU4D7Db023fFZvApelg=="]').click()
-
-
